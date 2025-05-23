ae96b4cb71f9e69f8d05c3bba3971cea9ad17bcf
Adrien LECOMTE <adrien@lecomte.org>
2025-05-23 09:03:33 +0200

    add database scripts

---
+++ database/migration/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO `image` (`image_id`, `path`) VALUES
+(1, '/test');
+
+INSERT INTO `users` (`user_id`, `email`, `username`, `password`, `bio`, `last_conection`, `image_id`) VALUES
+(1, 'test@gmail.com', 'test', 'password_text_1234', 'Ceci est une bio', '2025-05-23 08:47:23', 1);
